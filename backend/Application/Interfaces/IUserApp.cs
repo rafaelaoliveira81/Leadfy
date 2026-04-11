5d993deb7d8a9b2ba251abfdc0bf61f60ca97bc0
Rafaela Oliveira <rafaelaoliveiraalves91@outlook.com>
2026-04-11 13:31:29 -0300

    docs: adiciona documentação do código

--- backend/Application/Interfaces/IUserApp.cs
+++ backend/Application/Interfaces/IUserApp.cs
@@ -2,15 +2,15 @@ using Domain.Entities;
 
 public interface IUserApp
 {
-    Task<int> AddAsync(User user);
+    Task<int> AddAsync(User user, string password);
     Task<User> GetByIdAsync(int idUser);
     Task<User> GetByEmailAsync(string emailUser);
-    Task<User> GetByNameAsync(string nameUser);
+    Task<IEnumerable<User>> GetByNameContainingAsync(string nameUser);
     Task<IEnumerable<User>> GetAllAsync();
     Task<IEnumerable<User>> GetAllByStatusAsync(bool statusUser);
     Task UpdateAsync(User user);
     Task DeleteAsync(int idUser);
     Task DeactivateAsync(int idUser);
     Task ActivateAsync(int idUser);
-    Task UpdatePasswordAsync(User user, string currentPassword);
+    Task UpdatePasswordAsync(int userId, string currentPassword, string newPassword);
 }
