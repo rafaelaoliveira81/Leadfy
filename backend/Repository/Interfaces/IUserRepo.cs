c8c46211c11191d3bb93373b9aaa806e2b3eff0c
Rafaela Oliveira <rafaelaoliveiraalves91@outlook.com>
2026-04-11 13:29:26 -0300

    feat: implementa repositório do usuário

--- backend/Repository/Interfaces/IUserRepo.cs
+++ backend/Repository/Interfaces/IUserRepo.cs
@@ -4,7 +4,7 @@ public interface IUserRepo
 {
     Task<int> AddAsync(User user);
     Task<User> GetByIdAsync(int idUser);
-    Task<User> GetByNameAsync(string nameUser);
+    Task<IEnumerable<User>> GetByNameContainingAsync(string nameUser);
     Task<User> GetByEmailAsync(string emailUser);
     Task<IEnumerable<User>> GetAllAsync();
     Task<IEnumerable<User>> GetAllByStatusAsync(bool statusUser);
